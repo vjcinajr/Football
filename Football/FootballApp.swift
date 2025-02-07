8aab43d31f71d42e0b6fc44fdc409c0feaf9de5c
Vincent Cina <vcina@me.com>
2025-02-07 16:33:11 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by VINCENT CINA on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
